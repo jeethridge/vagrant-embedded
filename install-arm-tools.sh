cdcbfb0f31dcc13c2e8a8c98e5591b3256b342f2
James Ethridge <jeethridge@gmail.com>
2016-09-06 19:27:52 -0400

    add stm32 and stlink tool setup

--- install-arm-tools.sh
+++ install-arm-tools.sh
@@ -5,21 +5,19 @@ sudo add-apt-repository ppa:team-gcc-arm-embedded/ppa
 sudo apt-get update
 sudo apt-get install gcc-arm-embedded
 
-#allow user access to stlink usb devices
-sudo touch /etc/udev/rules.d/45-usb-stlink-v2.rules
-echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="0483", ATTR{idProduct}=="3748", MODE="0666"' | \
- sudo tee --append /etc/udev/rules.d/45-usb-stlink-v2.rules > /dev/null
-
-
 
 
 #STLINK Setup
-#cd /home/vagrant
-#git clone https://github.com/texane/stlink.git
-#cd stlink/
-#cmake -DCMAKE_BUILD_TYPE=Debug
-#make
-#why don't these two steps work?
+cd /home/vagrant
+git clone https://github.com/texane/stlink.git
+cd stlink/
+cmake -DCMAKE_BUILD_TYPE=Debug
+make
+
+#allow user access to stlink usb devices by copying the udev rules from stlink
+sudo cp /home/vagrant/stlink/etc/udev/rules.d/* /etc/udev/rules.d
+sudo udevadm control --reload-rules
+sudo udevadm trigger
 
 #clone the demo repository for STM32F4
-#git clone https://github.com/rowol/stm32_discovery_arm_gcc /vagrant/projects/stm32_discovery_arm_gcc
+git clone https://github.com/rowol/stm32_discovery_arm_gcc /vagrant/projects/stm32_discovery_arm_gcc
